6ceccf3e3b26b92d7552edaa949a2c2669a941d0
ROBERT97 <robert97@robert.com>
2020-03-20 15:03:18 +0200

    update

---
+++ Teme/src/Prob1/Array.java
@@ -0,0 +1,31 @@
+package Prob1;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Array {
+
+	public static void main(String[] args) {
+		double[] arr = {37.1,37.0,37.4,};
+		
+        ArrayList<Double> no = new ArrayList<Double>();
+        ArrayList<Double> ano = new ArrayList<Double>();
+		System.out.println("Temperaturile pacientilor : "+Arrays.toString(arr));
+		
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]>37.0) {
+				ano.add(arr[i]);
+				System.out.println("Pacientul cu nr: "+i+" neste nesanatos si are temperatura: "+arr[i]);
+			}else {
+				no.add(arr[i]);
+				System.out.println("Pacientul cu nr: "+i+" este sanatos si are temperatura: "+arr[i]);
+			}
+	
+		}
+		System.out.println("Sanatosi avem : " +no);
+		System.out.println("Nesanatosi avem : "+ano);
+		
+        }
+	}
+
+
